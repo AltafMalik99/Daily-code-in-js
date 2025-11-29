c8c2994c6de0d3358abeca7f4f24291f6470d250
Muhammad Altaf <zmaltaf99@gmail.com>
2025-11-30 00:19:20 +0500

    first commit

---
+++ class 1/index.js
@@ -0,0 +1,169 @@
+// 1st book name (A smart way to learn JavaScript)
+// 2nd book name (The Definative Guide)
+// 3rd book name (you don't know js)
+// 4th book name (the elouguent JavaScript)
+
+
+// alert("hello noor")
+
+// var hello = "hello malik"
+
+// console.log(alert);
+
+
+// 2. Data Types
+
+// String → "Hello"
+
+// Number → 15
+
+// Boolean → true / false
+
+
+// alert("hello world")
+
+// alert("hello")
+
+// console.log("hello");
+
+
+// () parenthesis
+// {} braces {scope}
+// [] bracket
+
+
+// alert (3)
+// var String = 1
+// console.log(typeof String);
+
+
+// console.log(" hello");
+
+// window.console.log("hello's'" )
+
+
+
+
+// string space .............
+
+//  String ke andar space character hota hai.
+
+// JavaScript us space ko exact waisa hi print karta hai jaise tum likhte ho.
+
+// String ke andar jitni marzi spaces daal lo — woh valid hoti hain.
+
+
+// data types
+
+
+
+// javascript data types wo hotay hain jo batate hain ke variable kis type ke data store karega. javascript me data types do category me hotay hotay hain
+
+// strting ("hello")
+// number (15)
+// boolean (true / false)
+
+
+
+
+
+
+// sir muzamil class 1......................
+
+
+// var num = false
+// alert(typeof (num));
+
+// var num_1=5;
+// var num_2=8
+// console.log(num_1+num_2);
+
+
+
+// var num_1;
+// num_1=3 
+// num_1=7;
+// console.log(num_1);
+
+
+// var num_1=9;
+// var num2=12;
+// console.log(num2 % num_1);
+
+
+// var num_1;  // declare kiya
+// num_1 = 2;  // value assign ki
+
+
+// bodmas  (Brackets ()   order (exponents,jaise **)  division /  multiplcation *  addition + subtration)
+
+
+// var a=10;
+// var b=20;
+// var c=a+b*3/2
+// console.log(c);
+
+
+
+// var a = 45;
+// var b ="5";
+// var c="5";
+// alert (c+ a +b)
+
+
+
+// increment operator
+
+
+// post-incerment (a++) pehle value return hoti hai , phir increment hota ha
+
+
+// let a= 3;
+// let b = a++;
+// console.log(a);
+
+// a++ ne pehle 5 return liya 
+// fir a ki value 6 ho gyai
+
+// Value phelay use hoti hai, phir barhti hai.
+
+
+
+
+
+
+// pre-increment(++)
+// pehle increment hota hai phir value return hoti hai
+
+
+// let a= 5;
+// let b = ++a;
+// console.log(a);
+// console.log(a++);
+
+// a pehle 6 bana
+// phir b me 6 assign hua
+
+// Value phelay barhti hai, phir use hoti hai.
+
+// var x=1;
+// x++;       //postincrement
+// ++x;       //preincremet
+
+
+// console.log(x);;
+
+// var x=1;
+// console.log(++x);
+// console.log(x++)
+// console.log(x)
+
+
+
+
+// peracts
+
+// var a= 5
+// var b = a++
+// console.log(b);
+
---
+++ class 2/index.js
@@ -0,0 +1,92 @@
+//String se pehle space allowed hota hai, string ke baad space bhi allowed hota hai.
+// JavaScript space ko ignore kar deta hai, jab tak wo quotes ke andar na ho.
+
+// agar string ka ander JavaScript unhe save karta hai, unka meaning hota hai.
+
+// Quotes ke bahar space ka koi meaning nahi hota
+// ";" ke baad space ho → koi farq nahi
+
+
+// String ke andar space = important
+// String ke bahar space = ignored (no effect)
+
+
+
+// keep in mind
+// 1.All are fundamentals
+// c , java , c# , phyton , javascript (variable)
+
+
+
+// website name peractic
+// programiz javascript
+
+
+
+
+
+// ..................chapter 2..............
+
+//........ variable for string.........
+
+// define ak martabe use or variable har jag use kare gyae
+
+// var name = "Mark"
+
+// is ma var keyword ha or name variable ka name
+
+// variable -> very --> (changable)
+// variable is like a container you can use it everywhere
+
+
+// javascript line by line chalti ha
+// javascript ma ak line ma error a gya to baki ka code nhi chal gya
+
+
+// javascrip ka file ka ander top pa sarif variable jati ha variable ka value nhi 
+
+
+// JavaScript me agar error aata hai, to uske baad ki lines execute nahi hoti
+
+// var name ="Altaf"
+// console.log(myName);
+// console.log(myName);
+// console.log(name);
+
+
+// var myName ="noor";
+// console.log(myName);
+
+
+// var aur let → value update ho sakti hai
+// const → value update nahi ho sakti (constant)
+
+
+// js ma hamesha last wala updated value show ho gye
+
+
+
+// important point
+
+/*Declaration = variable ka naam batana, value optional
+Agar value nahi di → JavaScript me value undefined hoti hai
+var → function scoped
+let → block scoped
+const → block scoped, value change nahi hoti*/
+
+
+
+// first initialize
+
+// const myName = "ali";
+
+// myName ="noor"
+// //reassign (just change variable string)
+
+// //reinitialze (also create with var keybword)
+// let myName = "bilal";
+// console.log(myName);
+
+
+
+// video 46:42
