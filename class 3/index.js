c57697755366c2d9808759e08bb99c59ce542772
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-02 00:55:02 +0500

    Maths class cover

--- class 3/index.js
+++ class 3/index.js
@@ -35,8 +35,59 @@ Console me jo print hota hai → return value hoti hai, variable ka final value 
 
 
 
-var a = 2
-console.log(a-- + a--);
-// var decre = a-- + a--
+// var a = 2
+// var decre = a-- - --a - --a - --a
+// // console.log(a-- + a-- );
+// console.log(a);
+
+
+var a = 1
+var b = 1
+
+
+
+
+/* a = 2
+
+ a--
+Post-decrement
+Pehle value use hoti hai, phir a = a - 1 hota hai.
+So
+Use value = 2
+New value = 1*/
+
+
+/* --a
+Pre-decrement
+Pehle a = a - 1, phir value use hoti hai.
+Current a = 1
+After decrement: a = 0
+Use value = 0*/
+
+
+// Example 2
+
+/*
+Pehle value change hoti hai, phir use hoti hai.
+
+var a = 5;
+console.log(++a);
+
+ Pehle a = 6
+Phir console.log(6)
+ Output: 6  */
+
+
+ /* Pehle value use hoti hai, phir change hoti hai.
+ 
+ var a = 5;
+ console.log(a++)
+
+ Pehle console.log(5)
+
+Phir a = 6
+Output: 5
+Final value of a = 6
+ */
 
 
