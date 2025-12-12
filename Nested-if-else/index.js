81c6215d9f08c8923fa56da29bed848af6bdff38
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-12 23:48:30 +0500

    Nasted if-else

---
+++ Nested-if-else/index.js
@@ -0,0 +1,118 @@
+// && tab true hoti hai jab dono condition true hon
+// || tab true hoti hai jab kam se kam ek condition true ho
+
+
+// Example
+
+// && (And)
+/*  
+true && true -> true
+true && false -> false
+fasle && true -> false
+false && fasle -> false
+
+
+//  || (or)
+
+true || flase -> true
+fasle || true -> true
+true || true -> ty true
+false | false -> false
+
+
+*/
+
+
+// && dono sahi honge to kam hoga
+// || ko ek sahi ho gya to kaam ho jayegya
+
+
+
+
+// &&
+// var age = 20
+// var city =  "karachi"
+// if(age >15 && city === "karachi"){
+//     console.log("allow");
+    
+// }
+
+// is ma 20 se choti number per true 20 se oper fale a gya
+
+
+/* ek condtion ke ander doosri condtion check karna 
+pehle if check ho gya agar wo true ho jaye
+tab uske ander wala nested if check hogya*/
+
+
+
+
+
+
+// var name = "noor"
+// var age = 20
+// if(name == "noor" && age == 20 ) {
+//     console.log("allow");
+    
+// }
+// is ko ak false be mile gya na to ya nhi chale gya
+
+
+
+// multi condition
+
+// var name = "ali"
+// var age = 20
+// var roll = 123
+// var sub = 7
+
+// if (name == "ali" &&  age == 20 || ( roll == 1213 && sub == 7 ) ){
+//     console.log("run");
+    
+// }
+
+
+
+// var name = "20"
+// var age = 20
+// var roll = 555
+//  var sub = 7
+
+// if(name <21 &&  (age > 20 && roll  < 555 || sub == 7 )){
+//     console.log("allow");
+    
+// }
+
+
+
+
+
+
+// if statements nested chapter 14......
+
+// is her ak condition check ho gya
+
+// var name = "ali"
+// var day = "monday"
+
+// if(name == "ali") {
+//     if (day == "monday") {
+//         console.log("welcome");
+        
+//     }else{
+//         console.log("not class today")
+//     } 
+//     }
+
+
+
+// var age = 20;
+// var city = "Karachi";
+
+// if (age >= 2) {           // Pehla IF age check kare ga
+//     console.log("Adult");
+
+//     if (city === "Karachi") {  // Agar pehla true ho to ye check hoga
+//         console.log("You live in Karachi");
+//     }
+// }
