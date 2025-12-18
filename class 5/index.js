8c8c8b17e6d609b4f06d2922f59bc1af888410ed
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-18 23:56:41 +0500

    for loop  % nested

--- class 5/index.js
+++ class 5/index.js
@@ -101,3 +101,110 @@ use oti ha jab tak koi condtion true ho */
 //     console.log("not found")
 // }
 // note without if ka else nhi chal sakta
+
+
+
+
+// peracties......
+
+// for (var i = 0; i <5;i++ )
+//     console.log(i);
+    
+/* for loop kisi kam ko bar bar repeat karne ke liye use hoti hai */
+
+
+
+
+
+// var found = false ;
+
+// var arr = [10,20,30,40]
+
+// for (var i = 0 ; i < arr.length; i ++){
+//     if (arr[i] === 43){
+//         found = true;
+//         break;
+//     }
+// }
+// console.log(found);
+
+
+// found ek flag hai
+
+
+// var fruits = ["apple" , "banana", "mango",];
+// console.log(fruits)
+
+
+
+// break
+
+// for (var i = 1; i <= 20; i++){
+//     if(i === 12){
+//         break
+//     }
+//     console.log(i);
+    
+// }
+
+
+
+/* 1 se number print karna lakin jaise hi  i = 5 hota hai loop wahi ruk jata ha  */
+
+
+
+
+// continue 
+
+// for (var i = 0 ; i <= 20; i++){
+//     if(i === 11 ){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+/* current loop iteration ko skip karta hai
+loop band nhi hoti sirf aik value chhor kar next pert chala jata hai */
+
+
+
+
+
+// ......chater 20 nested for loop ...........
+
+/* nested for loop ka matlab hota hai for loop ke ander for loop
+outer loop 1 dafa chalta hai 
+inner loop poor complete hoti ha */
+
+
+// for (var i = 1; i <= 5; i ++ ){  // outer loop
+//     for (var j = 1; j <= 10; j++){ // inner loop
+//         console.log(i + "x" + j + "=" + (i * j))
+//     }
+//     console.log("hello")
+// }
+
+
+
+// for (var i = 0; i < 3 ; i++){
+//     for(var j = 0 ; j< 3; j++){
+//         console.log("hello");
+        
+//     }
+// }
+
+
+
+
+
+
+// ..... chapter 21  changing case.......
+
+// var checkstudent = prompt ("enter your name")
+// var checkstudentwithCapitallLetter = checkstudent.toLowerCase()
+// var student = ["Ali", "Ahmed", "Noor", "Bilal", "ibrahim"];
+// for (var i = 0; i < student.length; i ++) {
+//     if(student[i] == checkstudent){
+//         console.log(checkstudent)
+//     }
+// }
