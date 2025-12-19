cc646b3bc0a1ea34d6fa48a7360637167914517c
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-20 00:42:14 +0500

    for loop & nested , string cases

--- class 5/index.js
+++ class 5/index.js
@@ -195,10 +195,69 @@ inner loop poor complete hoti ha */
 
 
 
+// var arr = [[1,2,3] , ["a","b","c"],["e","f"]];
+
+// for (var i = 0; i <arr.length; i ++){
+
+//     for(var j = 0; j <arr.length ; j++){
+
+//         console.log(arr [i]);
+//     }
+// }
+
+
+
+
+// for( var i = 0; i < 5 ; i++){
+// var star = '';
+//     for(var j = 0; j <=i; j ++){
+//         star = star + '*';
+//     }
+//     console.log(star);
+    
+// }
 
 
 
 // ..... chapter 21  changing case.......
+
+// js ma changing case ka matlab hoti hai text (string) ko uppercase ya 
+// lowercase ma badalna
+
+
+// var text = "HELLO WORLD";
+// var result = text.toLowerCase();
+// console.log(result);
+
+
+// var text = "altaf" ; 
+//  var result = text.toUpperCase();
+//  console.log(result)
+
+
+
+// var name = prompt ("enter your name")
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+
+
+
+// var city = "karachi";
+
+// if(city.toLowerCase() === "karachi"){
+//     console.log("Matched");
+    
+// }
+
+
+
+// var word = "altfa"
+// var captial = 
+// word.charAt(0).toUpperCase() + word.slice(1). toLowerCase();
+// console.log(captial);
+
+
 
 // var checkstudent = prompt ("enter your name")
 // var checkstudentwithCapitallLetter = checkstudent.toLowerCase()
@@ -208,3 +267,59 @@ inner loop poor complete hoti ha */
 //         console.log(checkstudent)
 //     }
 // }
+
+
+
+
+//........ chapter 22 Strings: Measuring and extracting parts
+
+
+// string ma kitne characters ha wo . lenght se pata chalta ha
+
+
+
+// var text = "pakistan"
+// console.log(text.length);
+
+
+
+// Extracting parts string ka kuchi parts nikalna
+
+
+// var city = "karacni";
+// console.log(city.slice(0,4))
+
+
+
+// var text = "pakistan"
+// var text =  prompt ("enter city name")
+// console.log(text.slice(0,3));
+
+
+
+// var text = prompt ("enter your city name");
+// var text2 = text.slice(0,1)
+// console.log(text2);
+
+
+
+// negative index 
+
+// var text = "pakistan"
+// var text = prompt ("enter your cite name")
+// console.log(text.slice(-7))
+
+
+
+// var text = "hello world"
+// console.log(text.substring(1,11))
+
+
+
+// var email = "example@gmail.com";
+// var userName = email.slice(0,email.indexOf("@"));
+// console.log(userName);
+
+
+
+// video 11
