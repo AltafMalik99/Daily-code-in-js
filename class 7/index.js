59ad36f8a45b1a26fe5188399e57fb8179d9b2fb
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-24 23:17:59 +0500

    Rounding number

---
+++ class 7/index.js
@@ -0,0 +1,19 @@
+// .....chapter 26 Rounding number ....
+
+
+// var mathematics = Math.random()
+// console.log("mathematics =>",mathematics);
+
+// is ma sar dafa refers kar na ap nay number generat ho gya
+
+
+//       0.             6444954135630876
+//    characterstics         mansissa
+
+// var Maths = Math.round (Math.random());
+// console.log(Maths);
+
+// is ma 1 or 0 ka demayan number a gye
+
+
+// video 19
