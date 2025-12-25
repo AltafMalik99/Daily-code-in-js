fee8aee30cc12234ddfde771a210eb75411dedf8
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-26 00:37:40 +0500

    Rounding & type conversion

--- class 7/index.js
+++ class 7/index.js
@@ -36,7 +36,7 @@
 /* ceil (Math.ceil) javascript ka function hai jo kisi nnumber ko uper wali poori ginti (greater integer) per le jata hai chahe decimal chhota ho ya bara.  */
 
 
-// var rounds = Math.ceil(1.1)  // output 4
+// var rounds = Math.ceil(3.1)  // output 4
 // console.log(rounds);
 
 // // hamesha uper round
@@ -66,10 +66,10 @@ round = qareebi number */
 unpredictably appear hai jise dice roll lotters number game scores */
 
 
-// var num = Math.floor(Math.random()* 10)
+// var num = Math.floor(Math.random() * 10)
 // console.log(num);
 
-
+// Har dafa run karne par random number change hoga.
 
 // var die = (Math.random()* 6) 
 // var dieWithfloor = Math.floor(die)
@@ -79,3 +79,53 @@ unpredictably appear hai jise dice roll lotters number game scores */
 
 
 //... chapter ..28....  coverting strings to integers and decimals....
+
+
+// coercion
+
+/* javascript ma coercion ka matlab hoti ha automatic type conversion yani 
+jab js khu ba khud ek value ka data type badal deti hai take code chal sage */
+
+// js kisi value ko apne aap number string ya boolean me convert kar leti hai
+
+
+// var result = "5" + 2;
+// console.log(result);
+
+// 5 ek string ha aur 2 number js automaticall 2 ko string me badal kar 52 bana data hai
+
+// var result = "5" - 2;
+// console.log(result);
+
+// 5 string thi js ne use number me convert kar diya ha
+
+
+
+// var age = prompt("enter a age")
+// var result = parseInt(age) + 1
+// console.log(result);
+// intergers ma be convert kar de gya 
+
+
+// var age = prompt("enter a age")
+// var result = parseFloat(age) + 1
+// console.log(result);
+
+// intergers ma be convert kar de gya or poor point ka bad wale value be de gya
+
+
+
+// chapter.. 29......... Converting strings to numbers, numbers to strings......
+
+// var number = 89
+// var str = number.toString()
+// console.log(typeof str);
+
+
+// yani jab koi text number me badalna ho
+
+
+// var str = "123"
+// var num = Number(str);
+// console.log(typeof num);
+
