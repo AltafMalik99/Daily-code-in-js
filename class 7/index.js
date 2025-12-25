5383b9a29041302c272581d55a2ef59d3447d20d
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-25 23:25:59 +0500

    Rounding & type conversion

--- class 7/index.js
+++ class 7/index.js
@@ -16,6 +16,66 @@
 // is ma 1 or 0 ka demayan number a gye
 
 
+// var Maths = Math.round(Math.random() * 10)
+// console.log(Maths);
 
 
-// video 19
+
+// ....nearest round..
+
+// “Nearest round” ka matlab hai number ko qareebi (sab se nazdeeki) poore number par round karna
+
+
+// var Maths = Math.round(2.5) // output 3
+// console.log(Maths);
+
+
+
+// ceil...
+
+/* ceil (Math.ceil) javascript ka function hai jo kisi nnumber ko uper wali poori ginti (greater integer) per le jata hai chahe decimal chhota ho ya bara.  */
+
+
+// var rounds = Math.ceil(1.1)  // output 4
+// console.log(rounds);
+
+// // hamesha uper round
+
+
+// var rounds = Math.floor(3.9)  // output 3
+// console.log(rounds);
+
+// hamesha neeche round
+
+// jo number ha os ka dermyan ma
+
+
+
+/* ceil = upar ki taraf
+floor = neeche ki taraf
+round = qareebi number */
+
+
+// stack overflow 
+
+
+
+// ..chapter..27...Generating random numbers...
+
+/*random number generate karna ka matlab hai aise numbers banana jo 
+unpredictably appear hai jise dice roll lotters number game scores */
+
+
+// var num = Math.floor(Math.random()* 10)
+// console.log(num);
+
+
+
+// var die = (Math.random()* 6) 
+// var dieWithfloor = Math.floor(die)
+// console.log("die",dieWithfloor);
+
+
+
+
+//... chapter ..28....  coverting strings to integers and decimals....
