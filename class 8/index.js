a4e1d1faea951140f3c073c952eb9608b4f6fdbc
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-30 00:47:31 +0500

    Data & time

--- class 8/index.js
+++ class 8/index.js
@@ -40,5 +40,34 @@ Aur string return karta hai */
 
 // .....chapter 31 Getting the current data and time ......
 
-var data = Date()
-console.log(data);
+// var data = Date()
+// console.log(data);
+
+
+// var date = new Date();
+// console.log("year=>",date.getFullYear());
+// console.log("date=>",date.getDate())
+// console.log("day=>",date.getDay())
+
+
+// var date = new Date();
+// console.log("date=>," ,date.getFullYear());
+// console.log("date=>", date.getDate());
+
+// var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday","saturday"];
+// console.log("day=>" , days[date.getDay()])
+
+
+// console.log("Month",date.getMonth());
+// console.log("hours", date.getHours());
+// console.log("Minutes",date.getMinutes());
+// console.log("second" ,date.getSeconds())
+// console.log("Millisecond", date.getMilliseconds())
+
+
+// pm or am
+// console.log(new Date().toLocaleString());
+
+
+// console.log(new Date().toLocaleString('en-US', { hour12: true }));
+
