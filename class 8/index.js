e1e468b9fb52d22b4825742eb2c175575aa94ca3
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-29 23:31:58 +0500

    Data & time

--- class 8/index.js
+++ class 8/index.js
@@ -1,1 +1,44 @@
-var text = prompt("Hello" , "wordl")
+// var number = 23.5457
+// var fixeNumber = number.toFixed(2)
+// console.log(number);
+// console.log(fixeNumber)
+
+/*1) toFixed()
+Sirf decimal places fix karta hai
+Aur string return karta hai */
+
+
+// 2 toPrecision()
+
+// var num1 = 23.3443
+// var num2 = num1.toPrecision(2)
+// console.log(num2);
+
+
+
+// var number2 = 23.5
+// var number2 = number2.toString()
+
+// if(number2.charAt (number2.length -1 ) == "5" ) {
+//     number2=number2.slice(0,number2.length - 1) + "6"
+//     console.log("number 2 ",number2);
+    
+// }
+
+
+// var number2 = 1.6;
+// number2 = number2.toString();
+
+// if (number2.charAt(number2.length - 1) == "5") {
+//     number2 = number2.slice(0, number2.length - 1) + "6";
+// }
+
+// console.log(number2);
+
+
+
+
+// .....chapter 31 Getting the current data and time ......
+
+var data = Date()
+console.log(data);
