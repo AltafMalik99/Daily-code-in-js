e331263b4a4a82d7ac4d48c66624e47076f0dd7f
Muhammad Altaf <zmaltaf99@gmail.com>
2025-12-31 22:38:21 +0500

    function

---
+++ class 9/index.js
@@ -0,0 +1,89 @@
+// ...........chapter 35 functions.............
+
+/* function ek aisa block hia jime code likha jata hai jo repeatable task perform karta
+hai aik baar function likho aur baar - baar use kae sakte ho  
+function code ko clean aur reusable banate hai*/
+
+
+// function sum() {
+//     console.log("hello world");
+// }
+// sum();
+// sum();
+
+
+
+// function sum(a,b) { // paremeters
+//     console.log(a+b)
+// }
+// sum(2,2); // aurguments
+// sum(10,20);
+// sum(20,30)
+// sum(50,50)
+// sum(20,12)
+
+
+
+
+// resturn...
+
+// function sum(a,b) {
+//     return a+b;
+// };
+// var abc=sum(5,2);
+// console.log(abc);
+
+
+// function  sum(a,b){
+
+//     return a+b
+// }
+// var abc=sum(5,2)
+// console.log(abc);
+
+
+function sum(a,b) {
+    return a+b
+}
+var abc=sum(10,20)
+console.log(abc)
+
+
+// function great(){
+//     console.log("Hello, Altaf");
+// }
+// great() //function call 
+
+
+// function functionName(){
+//     console.log("Hello, world!");
+    
+// }
+// functionName("")
+
+// function fun(fName, lName) {
+//     console.log(fName,lName);
+// }
+// var fName = "malik"
+// fun(fName, "altaf");
+
+
+// function func(lName,fName) {
+//     console.log(fName, lName);
+    
+// }
+// func("ibrahim" , "khan");
+
+
+// function add (a,b = )
+
+
+// function nice(name) {
+// console.log("my name " + name + " is malik altaf");
+// console.log("my name " +name +" is malik salman");
+// console.log("my name is malik Atif");
+// console.log("my name is malik noor");
+// console.log("my name is malik Aftab");
+// console.log("my name is malik Hamza");
+// }
+// nice("ayan")
