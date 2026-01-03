94378fdc18a222342236901877781764ace8fc28
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-03 20:43:40 +0500

    chapter 1 to 18

---
+++ class 10 Revision 1 - 5/index.js
@@ -0,0 +1,51 @@
+// alert ("hi Altf")
+
+// var                               name = "Altaf"
+// ya variable ka key word ha ,   ya variable name ha 
+
+
+// var name = "Altaf"
+// console.error(name);
+
+
+// var names = {
+//     names: "altaf",
+//     roll : 123,
+//     age : 18
+// };
+// const myName = "altaf";
+// const lastName = "malik"
+// console.table(names);
+
+
+// var myFirstName = "Altaf";
+// var myFirstName = "malik" // reinitialize
+// console.log(myFirstName);
+ 
+
+
+
+// chapter 5.... Math expressions Familiar operators
+
+// var num = 20
+// console.log(10+num);
+
+
+// var num1 = 10;
+// var num2 = 10;
+// var sum =  num1 + num2
+// console.log(sum);
+
+// var a = "2";
+// var b = "10";
+// console.log(a-b);
+
+
+
+// var num = 4
+// var num = 5
+// console.log(12+num);
+
+// var a = 23
+// var b = 10
+// console.log(a%b);
