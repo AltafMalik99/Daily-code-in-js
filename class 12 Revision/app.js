a21a0dc3e681660de327c5c902c0b96297b9c4cb
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-11 22:38:11 +0500

    22 TO 23

--- class 12 Revision/app.js
+++ class 12 Revision/app.js
@@ -46,20 +46,48 @@
 //     console.log("nhi mila")
 //  }
 
-var userName = prompt("enter your name ")
-var arr = ["saad","hamza","ali","asad"]
+// var userName = prompt("enter your name ")
+// var arr = ["saad","hamza","ali","asad"]
 
-var found = false
+// var found = false
 
-for (var i = 0 ; i<arr.length;i++){
+// for (var i = 0 ; i<arr.length;i++){
 
-    if(userName === arr[i]){
-found = true;
-break
-    }
-}if(found){
-    console.log("mil gaya ha " + userName);
+//     if(userName === arr[i]){
+// found = true;
+// break
+//     }
+// }if(found){
+//     console.log("mil gaya ha " + userName);
     
-}else{
-    console.log("nhi mil " )
-}
+// }else{
+//     console.log("nhi mil " )
+// }
+
+
+// var name  = "Ali"
+// console.log(name.toUpperCase());
+
+
+// var name  = "Ali"
+// console.log(name.toLowerCase());
+
+
+// only first word captail
+
+
+// var userName = prompt("enter a namr").toLowerCase();
+// var arr = ["faisal","ali","altaf"]
+
+// if(arr.includes(userName)){
+// console.log("ok mila gaya ha yar "+userName);
+// }else{
+//     console.log("nhi mila yar ")
+// }
+
+
+
+var text = prompt("enter a text");
+
+var result =  text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+console.log(result);
