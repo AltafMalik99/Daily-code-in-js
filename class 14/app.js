7ee4850344cc8edce0ed1bb37125489e65cdae77
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-18 22:42:40 +0500

    Mouse Event  & get , set input value

--- class 14/app.js
+++ class 14/app.js
@@ -43,6 +43,11 @@ jab tak condition true ho code repeat karta hai */
 
 // chapter 46..... Events: button
 
+
+/* jab user input box me type karta hai ya click karta hai
+tab js koi action karta hai */
+
+
 // function print(name){
 //     console.log("hello"+ name);
     
@@ -63,3 +68,95 @@ jab tak condition true ho code repeat karta hai */
 
 
 
+
+// chapter 49.... Reading field values:
+
+/* html input fileds textbox password email se user ka data js ma lena*/
+
+
+// function getvalue(){
+
+//     var inputs = document.getElementById("inputs");
+
+//     console.log(inputs.value);
+    
+// }
+
+
+// function getvalue (){
+//     var name = document.getElementById("username").value;
+//     console.log("user name is "+name);
+    
+// }
+
+// function getvalue (){
+//     var name = document.getElementById("username").value
+//     console.log("this code is run "+name);
+    
+// }
+
+// function checkpass(){
+//     var userpass = document.getElementById("pass").value;
+//     if(userpass === "1234"){
+//         console.log("corract password");
+        
+// }
+// else {
+//     console.log("incorract password")
+// }
+// }
+
+// function showcite(){
+//     var selectcity = document.getElementById("cite").value
+// console.log("this cite"+ selectcity);
+
+// }
+
+
+// .......chapter.... 50 setting field values
+
+// js se html ma input field ke ander value dalna
+// js ke through input box dropdown ma vlue set kar saktay ho
+
+
+// setvalue...
+
+// function setvalue(){
+//  var name =   document.getElementById("input").value = "Altaf"
+// console.log("this answer "+name);
+
+// }
+
+// ab jab button click hoga input box ma Altf likha hua ayega
+
+
+
+// function getvalue(){
+//     var name = document.getElementById("input")
+//     console.log(input.value);
+// }
+
+// function setvalue(){
+//     var name = document.getElementById("input")
+//     name.value = "Altaf"
+// }
+
+
+
+// function getvalue(){
+//     var name = document.getElementById("input")
+//     console.log(name.value);
+//    input.style.backgroundColor="lightblue";
+// }
+
+// function setvalue(){
+//     var name = document.getElementById("input")
+//     name.value = "Altaf"
+//     input.style.backgroundColor="red";
+// }
+
+// function bgred(){
+//     var input=document.getElementById("input")
+//     // input.style.backgroundColor="red";
+//     input.style.backgroundColor="yellow";
+// }
