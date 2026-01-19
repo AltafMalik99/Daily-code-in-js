00be7145bb5520d5a413facb3d204cd80aba4e1b
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-19 16:01:54 +0500

    Revision 40 to 50

---
+++ Pereties/app.js
@@ -0,0 +1,142 @@
+
+
+// switch ("wed"){
+
+// case "mon":
+// console.log("today is monday");
+
+// break
+
+// case "wed":
+// console.log("today is wedneday")
+
+// break
+
+// default :
+// console.log("not a class today")
+
+// }
+
+
+// var i = 3
+// do{
+//     console.log("hello",i);
+//     i++
+// } while(i >= 10)
+
+
+
+
+// var i = 1
+
+// do{
+
+//     console.log("hello "+i);
+// i++
+// }while(i<=12)
+
+
+
+
+// function add(a, b) {
+//     return a + b
+// }
+
+
+// function mult(a, b) {
+//     return a * b
+// }
+
+// function sub(a, b) {
+//     return a - b
+// }
+
+// function div(a, b) {
+//     return a / b
+// }
+
+
+// var num1 = + prompt("enter a first number", 2)
+// var num2 = + prompt("enter a second number", 2)
+// var operation = prompt("enter a perform sing (+,-,*,/)", '+')
+
+// switch (operation) {
+
+//     case "+":
+//         console.log(add(num1, num2));
+
+//         break
+
+//     case "*":
+//         console.log(mult(num1, num2));
+
+//         break
+
+//     case "-":
+//         console.log(sub(num1, num2));
+
+//         break
+
+//     case "/":
+//         console.log(div(num1, num2));
+
+
+// }
+
+
+// var corractPin = "123"
+// var userInput = prompt ("enter a coract pin")
+
+// while (corractPin !== userInput){
+//     userInput = prompt("wrong a incorract pin")
+// }
+// console.log("your pin is corract");
+
+// function print(){
+//     document.getElementById("print")
+//     console.log("Hello Altaf ");
+
+// }
+
+
+// function input(){
+//     document.getElementById("input")
+//     console.log("Altfa");
+
+// }
+
+
+
+// function getvalue(){
+//     var name = document.getElementById("input")
+//     console.log(name.value);
+// }
+
+// function setvalue(){
+//     var name = document.getElementById("input")
+//     name.value = "Hello Altaf"
+// }
+
+// function getvalue() {
+//     var name = document.getElementById("input")
+//     console.log(name.value);
+//     name.style.borderColor="blue"
+
+// }
+
+// function setvalue(){
+//     var name = document.getElementById("input")
+//     name.value = "altaf"
+//     name.style.backgroundColor="lightgreen"
+// }
+
+
+// function bacolor(){
+//     var name = document.getElementById("input")
+//     name.style.backgroundColor="yellow"
+// }
+
+// function changeimg(){
+//     var img = document.getElementById("img")
+//     img.src=("https://gtacars.ae/wp-content/uploads/2024/10/LEVA-GTA-Cars-JD-Urus-7-copy-scaled.jpg")
+// }
