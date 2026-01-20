2fd64bacd25e9e014847d3b9193e962d4171ede8
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-20 23:25:00 +0500

    Html document

--- class 15/app.js
+++ class 15/app.js
@@ -5,8 +5,6 @@
 // }
 
 
-// function showpara(){
-//     var p = document.getElementById("para").innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
-//     console.log(p);
-    
-// }
+
+
+// 1:2:00
