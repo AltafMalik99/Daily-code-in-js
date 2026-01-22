57a850b82c5e2fbdd2506c17c7ec3e65974647ff
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-23 00:50:42 +0500

    input field events

--- class 16/app.js
+++ class 16/app.js
@@ -5,4 +5,54 @@
 // }
 
 
-// video 13
+
+
+// function getvalue (){
+//     var name = document.getElementById("username")
+//         alert(name);
+//     }
+
+
+
+
+/* // video 13 */
+
+
+
+// function login(){
+//     var u = document.getElementById("user").value
+//      var p = document.getElementById("pass").value
+//       var e = document.getElementById("email").value
+
+
+// if(u === "abc" && p === "123" && e === "abc@gmail.com"){
+
+//     alert("login sucessful")
+// }else{
+//     alert("wrong")
+// }l
+
+// }
+
+
+
+
+
+// function check(){
+//     var e = document.getElementById("email").value;
+
+//     if(e === ""){
+//         alert("please email in the complete fill")
+//     }
+
+// }
+
+
+
+// function check(){
+//     var e = document.getElementById("email").value
+
+//     if(e === ""){
+//         alert("empty emial")
+//     }
+// }
