f614177b3a6b06b0b5fa6df716711c93b8b09dbd
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-26 00:11:36 +0500

    Target images Link with Events

---
+++ class 17/app.js
@@ -0,0 +1,31 @@
+// chapter 53 swapping images 
+
+/* do ya zyada images ke position / src ko ek duser ke sath replace karna
+images 1 aur images 2 hai button 1 ki jagah images 2 ajaye aur  */
+
+
+// function swap(){
+//     var images = document.getElementById("img").src
+//     var images = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqT6wVOAq7Runazz0KnwjcwDAnBgB5Xg_-OA&s"
+//     console.log("img=>", images);
+    
+// }
+
+// is na ma image updata kar dey
+
+
+// function swap(img,imgsrc){
+//     var images = document.getElementById(img).src = imgsrc
+//     console.log("img=>", images);
+    
+// }
+
+
+
+// function swap(){
+//     var a = document.getElementById("img1").src;
+//     document.getElementById("img1").src = document.getElementById("img2").src;
+//     document.getElementById("img2").src = a;
+// }
+
+// video 28
