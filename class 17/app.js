3568145dc279f74595280f4c5aa5cb1756b22051
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-26 23:30:42 +0500

    target images link with events

--- class 17/app.js
+++ class 17/app.js
@@ -21,11 +21,76 @@ images 1 aur images 2 hai button 1 ki jagah images 2 ajaye aur  */
 // }
 
 
-
 // function swap(){
 //     var a = document.getElementById("img1").src;
 //     document.getElementById("img1").src = document.getElementById("img2").src;
 //     document.getElementById("img2").src = a;
+//     console.log(a);
+    
 // }
 
-// video 28
+
+
+// function sawp(imageid){
+//     var images = document.getElementById(imageid).src = imageid
+//     console.log("pic+>" ,images);
+    
+// }
+
+
+
+/*is ma .src ka kam ha images ka source url set ya read karna ke liye hoti hai 
+.src ka  kam ha image ko change karna ya current images ka url dekhan hai */
+
+
+
+// function swap(imgId, newSrc){
+//    var newimg = document.getElementById(imgId).src = newSrc;
+//     console.log("Image changed to:", newimg);
+// }
+
+
+// function changeimg(){
+//     var img = document.getElementById("img")
+//     console.log("this img",img);
+    
+// }
+
+
+// ...............
+
+// function changeimg(){
+//     var img = document.getElementById("img")
+//     console.log("this img",img);
+    
+// }
+
+// function changeimg (){
+//     var address = document.getElementById("imglink")
+//     var address = address.href
+//     console.log(address)
+// }
+
+
+
+// // function big(){
+// //     var para = document.getElementById('para')
+// //     para.className += " size"
+// //     console.log("para",para)
+// // }
+
+
+
+// function big(){
+//     var para = document.getElementById('para').style.
+//     fontSize='50px'
+//     // para.className += " size"
+//     console.log("para",para)
+// }
+
+// function changeimg (){
+//     var address = document.getElementById("imglink")
+//     var address = "https://google.com"
+//     console.log(address)
+// }
+// chapter 54 sawapping imges and setting classes
