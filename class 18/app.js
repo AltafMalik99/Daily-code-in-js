3260a3bc1a0941d28571a5ca449972fd7b927945
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-29 23:55:44 +0500

    Dom  intro

---
+++ class 18/app.js
@@ -0,0 +1,35 @@
+// chapter 58 The Dom........... 
+
+// introduction in Dom
+
+// Document objact model
+
+/* html ko js ke through control karna Dom hai
+browser jab html load karta ha to html ko tree stucture bana deta ha
+har tag = objact / node
+har element js ma objact hai */
+
+
+// function chtext(){
+//     document.getElementById('dom').innerText = 'text change'
+// }
+
+// function chtext(){
+//     console.log(document);
+    
+// }
+
+
+// function chtext(){
+//  var p = document.childNodes[1].childNodes[2].childNodes[1]
+//  console.log(p);
+
+// }
+
+
+// function chtext(){
+//  var p = document.childNodes[1].childNodes[2].childNodes[5]
+//  console.log(p);
+//  p.innerHTML = "hello"
+
+// }
