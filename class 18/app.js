91967b4db4d1f855a689e6b2a328cc73147196de
Muhammad Altaf <zmaltaf99@gmail.com>
2026-01-31 19:36:17 +0500

    Dom

--- class 18/app.js
+++ class 18/app.js
@@ -28,8 +28,70 @@ har element js ma objact hai */
 
 
 // function chtext(){
-//  var p = document.childNodes[1].childNodes[2].childNodes[5]
+//  var p = document.children[0].children[1].children[2]
 //  console.log(p);
 //  p.innerHTML = "hello"
 
 // }
+
+
+
+
+// chapter 59... The DOM: Parents and children
+
+/* dom asal ma html ka tree structure hoti hai upar wala parent neeche wala children */
+
+
+// <div id="box">
+//   <h1>Hello</h1>
+//   <p>Welcome</p>
+// </div>
+
+
+// div (parent)
+//  ├── h1 (child)
+//  └── p  (child)
+
+
+
+// function check(){
+//     var p = document.getElementById("box");
+//     console.log(p.children);
+ 
+    
+//     var child = p.children[0];
+//     console.log(child.parentElement)
+
+// }
+ 
+
+// function test(){
+//   var parent = document.getElementById("list");
+
+//   console.log(parent.firstElementChild); // Apple
+//   console.log(parent.lastElementChild);  // Mango
+
+//   parent.firstElementChild.style.color = "red";
+// }
+
+
+
+// function test(){
+//     var parent = document.getElementById("list");
+    
+//     console.log(parent.firstElementChild);
+//     console.log(parent.lastElementChild);
+
+// parent.firstElementChild.style.color = "red"
+// parent.lastElementChild.style.color = "yellow"
+    
+// }
+
+
+// The DOM: Finding children
+
+/* dom (document object model) browser ka bana hua system hoti hai
+jo html ko js ka sath connect karta hai */
+
+/*Dom js ko power deta hai ke wo html ko dekha samajh aur change kar sakay */
+
