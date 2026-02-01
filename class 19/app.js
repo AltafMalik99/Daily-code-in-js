896167e362fcfe272913e1f1f52dd49741127eed
Muhammad Altaf <zmaltaf99@gmail.com>
2026-02-01 23:33:47 +0500

    Dom , Junk Artifacts

---
+++ class 19/app.js
@@ -0,0 +1,20 @@
+// chapter 61 The Dom junk artifacts and node Type ..
+
+/* Dom jab html ko read karta hai to har cheez ko node bana deta ha sarif tags hi nhi
+is wajah se kuchi extra / unwanted nodes ban jate hai jinhein hum junk artifacts kehte hai */
+
+// node 
+/* har extra text nodes hi junk artifacts hoti hai */
+
+// Element.children
+// children sirf html element deta hai junk text nodes nhi
+
+
+// NodeTypes kay hoti hai
+/* har dom node ka ek type number hoti hai jisse pata chalta hai ke node kis type ka hai */
+
+
+// var p = document.getElementById('para')
+// // console.log(p.nodeName);
+// console.log(p.firstChild.nodeType);
+
