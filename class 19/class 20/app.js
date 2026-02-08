f69074bc095ce15e6d5ef8937a89b9c55d7fcb13
Muhammad Altaf <zmaltaf99@gmail.com>
2026-02-09 00:10:00 +0500

    object part 1

---
+++ class 19/class 20/app.js
@@ -0,0 +1,25 @@
+// بسم اللہ
+
+// chapter 69 object..
+
+/* object aik asi cheez ha jisme multiple value hoti hai
+jaise real life ma , insaan -> name , age , city */
+
+// var arr = ["a","b",2,true]
+// console.log(arr);
+
+// var stdata = ["ali",10,20,"male"]
+
+// console.log(stdata[3]);
+
+
+// var stdata = {roll : 20, age: 20, gender: "male", name: "saad",city: "karachi"}
+// // console.log(stdata);
+// console.log(stdata['age']);
+
+
+// video 15
+
+/* now every have key (10 have key called roll (value)) */
+
+// var stdata = 
