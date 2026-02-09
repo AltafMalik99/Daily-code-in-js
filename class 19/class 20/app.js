60e1a968769ba34ec7d9ad62d730e09f5bb23d77
Muhammad Altaf <zmaltaf99@gmail.com>
2026-02-09 23:45:54 +0500

    object part 1

--- class 19/class 20/app.js
+++ class 19/class 20/app.js
@@ -13,9 +13,21 @@ jaise real life ma , insaan -> name , age , city */
 // console.log(stdata[3]);
 
 
-// var stdata = {roll : 20, age: 20, gender: "male", name: "saad",city: "karachi"}
-// // console.log(stdata);
+// var stdata = '{"roll": 10, "age": 20, "gender": "male", "name": "saad","city": "karachi" }';
+// console.log(stdata)["male"];
 // console.log(stdata['age']);
+
+
+// var person = {name: "Altaf" , gender: "male", city: "karachi", age: "20" };
+// console.log(person);
+
+
+var sdata = {
+    'name': "Ali",
+    "age": 20,
+    "subjact": "math",     
+}
+console.log(sdata["name"]);
 
 
 // video 15
