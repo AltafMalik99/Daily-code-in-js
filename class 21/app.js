81689d68affa255f0aa32bb8bcf3db8438b01956
Muhammad Altaf <zmaltaf99@gmail.com>
2026-02-23 00:34:51 +0500

    object exection context

--- class 21/app.js
+++ class 21/app.js
@@ -49,13 +49,34 @@
 // console.log(obj.speed());
 
 
-var obj = {
-    name: "malik",
-    age: 18,
-    speed: function =>(){
-     return   console.log(`my name is ${this.name} and my age is ${this.age}`);
+// var obj = {
+//     name: "malik",
+//     age: 18,
+//     speed: () =>{
+//      return   console.log(`my name is ${this.name} and my age is ${this.age}`);
         
-    }
-}
-console.log(obj.speed());
+//     }
+// }
+// obj.speed();
 
+
+
+// var obj ={
+//     name:"noor",
+//     age: 20 
+// }
+
+// var test = obj;
+// console.log(obj.age);
+
+
+
+
+// function Student(name, age , roll){
+// this.name = name
+// this.age = age
+// this.roll = roll
+// }
+
+// var student1 = new Student("ali", 30, 20)
+// console.log(student1);
