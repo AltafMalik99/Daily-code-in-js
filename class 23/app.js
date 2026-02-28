c3b3cc7ef8126bfddff82cc6b581853207a3074e
Muhammad Altaf <zmaltaf99@gmail.com>
2026-03-01 00:13:31 +0500

    local Storage

--- class 23/app.js
+++ class 23/app.js
@@ -8,3 +8,24 @@
 // localStorage.setItem("altaf")
 // var user = localStorage.getItem("username");
 // console.log(user);
+
+
+// localStorage.setItem("userName", "Ali")
+
+// var user = localStorage.getItem("username");
+// console.log(user);
+
+
+// setitem.............................................
+
+// localStorage.setItem("name", "alatf")
+
+// setitem ka kam data ko browser me sava karna
+
+
+// getitem......................................
+
+// localStorage.setItem("name","Altfa")
+
+// var user = localStorage.getItem("name")
+// console.log(user);
