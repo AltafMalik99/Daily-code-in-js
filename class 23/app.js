052d2a70fd1d69fe8cd0a59540c6966280b2f686
Muhammad Altaf <zmaltaf99@gmail.com>
2026-03-02 01:06:08 +0500

    local Storage part 1

--- class 23/app.js
+++ class 23/app.js
@@ -29,3 +29,32 @@
 
 // var user = localStorage.getItem("name")
 // console.log(user);
+
+// data read karta ha
+
+
+// Removeitem .......................................
+
+// localStorage.setItem("name1","noor")
+// localStorage.setItem("name2","malik")
+// localStorage.setItem("name5","hussan")
+// localStorage.setItem("name3","amhed")
+// localStorage.setItem("name4","aman")
+// localStorage.removeItem("name")
+// localStorage.removeItem("name1")
+
+// "name" key ko delete kar dega 
+
+
+// ................assigiment project..................................................................................................
+
+// function register(){
+//     var name = document.getElementById("login-name")
+//      var email = document.getElementById("login-email")
+//      var password = document.getElementById("login-password")
+// console.log(name.value , email.value, password.value);
+
+//     }
+
+
+// video 7
