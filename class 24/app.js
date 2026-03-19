2671a6e1e0c5948a488c5f65404302205f82b640
Muhammad Altaf <zmaltaf99@gmail.com>
2026-03-20 00:53:00 +0500

    Browser

---
+++ class 24/app.js
@@ -0,0 +1,62 @@
+// // chapter 76.. Browser control Getting and setting the URL
+
+// jo page open hai uska ka link hasil karna
+
+
+// console.log(window.location.href);
+
+
+// window.location.href = "https://google.com";
+// user automatically google per chala jayega
+
+// window.location.href = "https://youtube.com"
+
+//  window.location.href = "https://facebook.com";
+
+// window.location.reload();
+// page reload karna
+
+// window.history.back()
+// back page jana
+
+
+// window.history.forward();
+// next pages jana
+
+
+
+// chapter 77.. Browser control Getting and setting the URL another way
+
+
+// window.location.assign("https://google.com");
+
+// yeh bhi url change karta hai back to back ja sakta ha
+
+
+// window.location.replace("https://google.com")
+
+
+// chapter 78.. Browser control:Forward and reverse
+
+// ya browser ki history ko control karta ha jaise chrome ka back / forward button
+
+// Reverse....
+
+// window.history.back();
+
+// window.history.forward()
+
+
+// window.location.href = "index.html";
+
+// function nextpages(){
+//     window.location.href = "page1.html";
+// }
+
+// function goback(){
+//     window.history.back();
+// }
+
+
+
+
