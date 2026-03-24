5bab73f6b1df83a3dceba4c3a68165d0258a4c55
Muhammad Altaf <zmaltaf99@gmail.com>
2026-03-24 23:49:41 +0500

    Browser window validations

---
+++ class 25/app.js
@@ -0,0 +1,27 @@
+//  var a = window.open();
+// console.log(a);
+
+// ek new tab popup window kholta hao 
+// is se google ma new tab open ho jayega
+
+
+
+// location.reload()
+// ya current pages ko reload refresh karta hai
+
+// location.reload(true);
+// purana cacha ignore karke fresh data load karta hai (moder browsers ma mostly ignore ho sakta hai)
+
+
+
+// var a = window.open();
+// a.document.write("hello")
+// console.log(a);
+
+
+
+// var a = window.open("quiz.html","quiz","width=500,height=300");
+// console.log(a);
+
+
+// video 14
