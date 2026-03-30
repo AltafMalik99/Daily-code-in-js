8e7d1cbc6b8a611b77a74eaf4f9cc5841f19b46c
Muhammad Altaf <zmaltaf99@gmail.com>
2026-03-31 01:56:18 +0500

    Browser window validations

--- class 25/app.js
+++ class 25/app.js
@@ -20,8 +20,58 @@
 
 
 
-// var a = window.open("quiz.html","quiz","width=500,height=300");
+// var a = window.open("quiz.html","quiz","width=500,height=300,left=500,top=200");
 // console.log(a);
 
 
-// video 14
+
+// function checkLastName(){
+//     if(document.getElementById("lastNameField").ariaValueMax.length === 0){
+//         alert("please enter a last name");
+//         return false;
+//     }
+// }
+
+
+// let name = "   altaf malik     ";
+// let className = name.trim();
+// console.log(className);
+
+
+// spaces ha start aur end per trim un spaces ko remove kar deta ha 
+// trim sirf start ma end ke space remove karta ha
+
+
+
+// console.log("hello 1");
+
+// try{
+// consol.log("hello 2");
+// }catch(error){
+//     console.log("error");
+    
+// }
+// console.log("hello 3");
+
+// yaha wo code likho jahan errror a sakta hai
+// agar error ayya to yeh chalega
+
+
+
+
+// console.log("hello 1");
+// var name = ""
+
+// try{
+// if(name === ""){
+// throw"please enter your name"
+// }
+// }
+// catch(err){
+//     console.log(err);
+    
+// }
+// console.log("hello 3");
+
+
+// video 56
