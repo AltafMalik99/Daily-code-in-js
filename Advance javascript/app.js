fe7a6ce0f488fbdff62401c69d8fbc13072e2407
Muhammad Altaf <zmaltaf99@gmail.com>
2026-04-02 01:23:04 +0500

    Advance javascript

--- Advance javascript/app.js
+++ Advance javascript/app.js
@@ -1,3 +1,47 @@
 // Ems 6
 
-// video 21
+/* advance js programming ko zayada powerfull aur secalable banata hi jaise
+Asynchronous programming (promies async / await) 
+object - oriented programming (classes . inheritance)
+Function programming concepts (map ,filter, reduce)
+Modules & import /export
+Event loop , closures , scope
+Error handling (try/catech)
+
+ */
+
+
+// let a = "altaf"
+
+// var a = "noor"
+// console.log(a);
+
+
+// let se variable hoti hai value change ho sakti hai
+// block-scoped hoti hai => ke andar accessible bahar nhi
+
+
+
+// let age = 15;
+// // age = 10; value change ho gayi
+// console.log(age); // 10
+
+
+
+// if(true){
+//     let name = "altaf"
+//     // console.log(name); // altaf
+// }
+// console.log(name); // error ,
+
+
+
+// const se constant variable declare hoti hai
+
+// if(true){
+//     let a = "altaf"
+//     console.log(a);
+// }
+
+
+// video 32
