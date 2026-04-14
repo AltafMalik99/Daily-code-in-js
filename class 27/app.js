d6c03c17c1fa4d8a9d9ac22010fed4dddd767bd3
Muhammad Altaf <zmaltaf99@gmail.com>
2026-04-15 00:36:55 +0500

    class 2 advance  js

--- class 27/app.js
+++ class 27/app.js
@@ -1,6 +1,29 @@
 
-function data () {
-    const person = "noor"
-    console.log(person``);
+// function data () {
+//     const person = "noor"
+//     console.log(person);
     
-}
+// }
+
+
+// let obj = {name: "ali"}
+
+// let obj1 = obj
+
+// let obj2 = obj1;
+
+// obj.name = "noor"
+// console.log(obj,obj1,obj2);
+
+// matlab teeno variables ek hi object ke copy by reference hai
+
+
+let obj = {name: "ali"}
+
+let obj1 = obj
+
+let obj2 = {..."noor"}
+
+
+obj.name = "noor"
+console.log(obj,obj1,obj2);
