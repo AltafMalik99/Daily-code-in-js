13a6253094b6e2ae2f2c73fabadff8704d322461
Muhammad Altaf <zmaltaf99@gmail.com>
2026-04-16 23:49:57 +0500

    shallow copy nested

--- class 27/app.js
+++ class 27/app.js
@@ -18,12 +18,42 @@
 // matlab teeno variables ek hi object ke copy by reference hai
 
 
-let obj = {name: "ali"}
 
-let obj1 = obj
-
-let obj2 = {..."noor"}
+// shallow copy..............................................................
 
 
-obj.name = "noor"
-console.log(obj,obj1,obj2);
+// let obj = {name: "ali"}     // ek object bana memory me
+
+// let obj1 = obj      //copy nhi bani sirf reference mila obj aur obj1 dono same object point kar rhe hai
+
+// let obj2 = {...obj1};   // yaha spread operator use hua ab ek naya object create hua shallow copy
+
+// obj2.name = "noor" // sirf obj2 change hua original obj aur obj1 per koi effect nhi 
+
+// console.log(obj,obj1,obj2);
+
+
+
+// let obj = {
+//     name: "ali",
+// school: {
+//     name: "smit"
+// }
+// } ; 
+
+
+// let obj1 = obj    
+
+// let obj2 = JSON.parse(JSON.stringify(obj));  
+
+// obj2.school.name = "saylani"
+
+// // obj2.name = "noor" 
+
+// console.log(obj,obj1,obj2);
+
+
+// shallow copy 
+// object ya array ki copy banti ha lekin ander ka nested ander wala same refercece prt rehte hai
+
+
