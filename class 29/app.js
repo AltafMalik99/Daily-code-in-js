b0dd13478a186b0360240e81a22cb355bb60ab7b
Muhammad Altaf <zmaltaf99@gmail.com>
2026-04-20 00:45:15 +0500

    map foreach loop

---
+++ class 29/app.js
@@ -0,0 +1,34 @@
+
+// foreach
+
+// foreach kuch retur nhi kar ta
+
+// let number = [1,2,3,4]
+// number.forEach(function (num){
+//     console.log(num);
+    
+// })
+
+// let arr = ["karachi","islamabad","lahore"]
+
+// arr.forEach(function (elem,i,all){
+//     console.log(elem,i,all);
+    
+// })
+
+
+// let arr = ["karachi","islamabad","lahore"]
+
+// let result =  arr.map(function (elem,i){
+//     return `${elem}, "pakistan`
+// })
+// console.log(result);
+
+
+\
+
+
+// foreach array ka method hai jo har element per ek function run karta hai 
+// ya mainly loop chalane ke liye use hota hai
+
+//  1:23
