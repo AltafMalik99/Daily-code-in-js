0542b4f5a9c8a3b5ccd8c2e796be579c1027e62a
Muhammad Altaf <zmaltaf99@gmail.com>
2026-04-22 00:33:27 +0500

    advance filter

---
+++ class 30/app.js
@@ -0,0 +1,20 @@
+// let number = [1,2,3,4,5,6];
+// let even = number.filter(num=> num % 2 === 0)
+// console.log(even);
+
+// is ma agar number 2 se divide ho kar remainder 0 de --> even hai
+
+
+// let age = [12,21,22,17]
+// let result = age.filter(num=> num >= 18)
+// console.log(result);
+
+
+// let users = [
+//   {name: "Ali", active: true},
+//   {name: "Ahmed", active: false},
+//   {name: "Sara", active: true}
+// ];
+
+// let result = users.filter(user => user.active === true)
+// console.log(result);
