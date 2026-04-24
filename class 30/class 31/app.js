a269f53e13fd59697379f5e47bf22dc6465aaf1d
Muhammad Altaf <zmaltaf99@gmail.com>
2026-04-25 00:01:27 +0500

    oop incomplete

---
+++ class 30/class 31/app.js
@@ -0,0 +1,49 @@
+// oop (
+//  object oriented programming
+
+// is ks matlab hoti ha code ko through organize karna
+
+
+// let car  = {
+//     name: "civic",
+//     color: "white",
+//     start: function(){
+//         console.log("car start ho gay");
+        
+//     }
+// }
+// car.start()
+
+
+// oop ke 4 main concepts 
+
+// 1 Encapsulation
+// data function ek jagah rakhna
+
+// 2  Abstraction
+// sirf zaroori cheez dikhana
+// details hide karne 
+
+// 3  inheritance
+// ek object dusre se cheezain le sakta hai
+
+// 4  polymorphism
+// ek function alag tareeke se behave kare 
+
+
+
+// class car {
+//     constructor (name,color){
+//         this.name = name;
+//         this.color = color
+//     }
+//     start(){
+//         console.log(this.name + "start ho gayi ha");
+        
+//     }
+// }
+
+// let car1 = new car ("civic ", "black")
+// car1.start()
+
+
